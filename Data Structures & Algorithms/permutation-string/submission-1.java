9f0a231c6bc24f4844e950049b30f30bf7582c90
srishtibhatt <103835259+srishtibhatt@users.noreply.github.com>
2026-06-09 13:29:30 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1Count=new int[26];
+        int[] s2Count=new int[26];
+        for(int i=0;i<s1.length();i++){
+            s1Count[s1.charAt(i)-'a']++;
+        }
+
+        int widSize=s1.length();
+        for(int i=0;i<s2.length();i++){
+            s2Count[s2.charAt(i)-'a']++;
+            if(i>=widSize){
+                s2Count[s2.charAt(i-widSize)-'a']--;
+            }
+            if(isSame(s1Count,s2Count)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isSame(int[] s1,int[] s2){
+        for(int i=0;i<26;i++){
+            if(s1[i]!=s2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
